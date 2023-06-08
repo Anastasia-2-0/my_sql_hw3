64849814664f7edc69665bde146543da1f541856
Anastasia-2-0 <nosova0702@mail.ru>
2023-06-08 13:11:50 +0300

    finich

---
+++ задание1.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT city, sname, snum, comn FROM SALESPEOPLE
+LIMIT 0, 1000
+
+-- Date: 2023-06-08 11:15
+*/
+INSERT INTO `` (`city`,`sname`,`snum`,`comn`) VALUES ('London','Peel',1001,'0.12');
+INSERT INTO `` (`city`,`sname`,`snum`,`comn`) VALUES ('San Jose','Serres',1002,'0.13');
+INSERT INTO `` (`city`,`sname`,`snum`,`comn`) VALUES ('London','Motika',1004,'0.11');
+INSERT INTO `` (`city`,`sname`,`snum`,`comn`) VALUES ('Barcelona','Rifkin',1007,'0.15');
+INSERT INTO `` (`city`,`sname`,`snum`,`comn`) VALUES ('New York','Axelrod',1003,'0.10');
